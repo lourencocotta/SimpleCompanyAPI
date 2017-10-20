3be5336f0b778d8524630f9988fc1f011ada3f12
lourencocotta <lourenco.cotta@gmail.com>
2017-10-20 19:08:26 +0100

    Add Files

---
+++ CompanyAPI.Core/Contracts/DomainEventBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyAPI.Core.Contracts
+{
+    public abstract class DomainEventBase
+    {
+        public DateTime DateOccurred { get; protected set; } = DateTime.UtcNow;
+    }
+}
